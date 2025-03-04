b1bed3ef3309038b7910852ebeb94b6d63b0ee67
morenoide <alejandro.moreno3@estudiant.uib.cat>
2025-03-04 16:15:11 +0100

    0.1v
    Spring initializer

---
+++ src/main/java/com/adondeband/adondeband_api/AdondeBandApiApplication.java
@@ -0,0 +1,13 @@
+package com.adondeband.adondeband_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdondeBandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdondeBandApiApplication.class, args);
+	}
+
+}
