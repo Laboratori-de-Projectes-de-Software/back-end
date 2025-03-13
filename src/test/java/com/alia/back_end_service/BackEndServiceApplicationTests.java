af6d69cbd458ee334267db1c0e8a4301ad7f0650
RamienM <ruben.ramis1@estudiant.uib.eu>
2025-03-13 17:12:18 +0100

    Inicio

---
+++ src/test/java/com/alia/back_end_service/BackEndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alia.back_end_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
