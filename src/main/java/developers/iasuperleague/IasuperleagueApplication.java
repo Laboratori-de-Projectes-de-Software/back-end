60ce033b4e94cebcd15bc1895311621c1a94f832
Joan <joan.catala1@estudiant.uib.cat>
2025-03-19 18:23:11 +0100

    Configuración inicial Springboot

---
+++ src/main/java/developers/iasuperleague/IasuperleagueApplication.java
@@ -0,0 +1,13 @@
+package developers.iasuperleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasuperleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasuperleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/developers/iasuperleague/IasuperleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package developers.iasuperleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasuperleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
