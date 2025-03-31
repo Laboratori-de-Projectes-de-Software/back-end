9fe76080a27eaf80003c151eeed2c73d0f7e08ad
toni <tonifg2003@gmail.com>
2025-03-31 18:54:25 +0200

    Add a bot-filtered request (add peticion de bots con filtro)

--- src/main/java/org/example/backend/databaseapi/application/port/in/bot/BuscarAllBotsPort.java
+++ src/main/java/org/example/backend/databaseapi/application/port/in/bot/BuscarAllBotsPort.java
@@ -1,10 +1,12 @@
 package org.example.backend.databaseapi.application.port.in.bot;
 
 import org.example.backend.databaseapi.domain.bot.Bot;
+import org.example.backend.databaseapi.domain.bot.BotsFilterRequest;
 
 import java.util.List;
 
 public interface BuscarAllBotsPort {
 
     List<Bot> buscarAllBots();
+    List<Bot> buscarAllBotsFiltro(BotsFilterRequest request);
 }
