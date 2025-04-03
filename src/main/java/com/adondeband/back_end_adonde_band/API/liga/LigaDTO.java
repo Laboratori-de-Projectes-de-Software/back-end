697853b17ada68cd5cce8026c24ceafa233c609c
eduOsuna <95355480+eduOsuna@users.noreply.github.com>
2025-04-03 17:06:27 +0200

    Update LigaDTO.java
    cambios ignorables

--- src/main/java/com/adondeband/back_end_adonde_band/API/liga/LigaDTO.java
+++ src/main/java/com/adondeband/back_end_adonde_band/API/liga/LigaDTO.java
@@ -1,26 +1,29 @@
 package com.adondeband.back_end_adonde_band.API.liga;
 
+import com.adondeband.back_end_adonde_band.API.imagen.ImagenDTO;
 import com.adondeband.back_end_adonde_band.dominio.estado.ESTADO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LigaDTO {
 
-    // Agregar setter
     private String nombre;
-    // Agregar setter
+    private String urlImagen; // luego quizá se convierta en ImagenDTO
     private ESTADO estado;
-    // Agregar setter
     private LocalDateTime fechaInicio;
-    // Agregar setter
     private LocalDateTime fechaFin;
 
     public LigaDTO(String nombre, LocalDateTime fechaInicio, LocalDateTime fechaFin) {
         this.nombre = nombre;
+        urlImagen = "";
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
         this.estado = ESTADO.PENDIENTE;
