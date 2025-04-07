08b2ac1cdb2b14fc4a90813f4ab9f9a8db054a85
Alejandro Soto <106865529+AlejandroSotoGit@users.noreply.github.com>
2025-04-07 19:46:54 +0200

    inicio lógica ver liga

---
+++ src/main/java/com/example/back_end_eing/dto/botLeagueDTO.java
@@ -0,0 +1,12 @@
+package com.example.back_end_eing.dto;
+
+public class botLeagueDTO {
+    private Integer posicion;
+    private String nombre;
+    private String cualidad;
+    private String imagen;
+    private Integer total;
+    private Integer victorias;
+    private Integer derrotas;
+    private Integer empates;
+}
