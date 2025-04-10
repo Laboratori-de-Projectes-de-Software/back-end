27e0a3bb6bf3645776b952ab8c57befeebc6cf9b
mateu <m.segui1@estudiant.uib.cat>
2025-04-10 21:34:16 +0200

    Mis bots + Detalles bot

--- src/main/java/com/example/back_end_eing/services/BotService.java
+++ src/main/java/com/example/back_end_eing/services/BotService.java
@@ -1,7 +1,16 @@
 package com.example.back_end_eing.services;
 
+import com.example.back_end_eing.dto.BotResponseDTO;
+import com.example.back_end_eing.dto.BotSummaryResponseDTO;
+
+import java.util.List;
+
 public interface BotService {
     
     public void BotRegistro(String nombre, String descripcion, String foto, Integer victorias, Integer numJornadas, String API, Long id);
 
+    List<BotSummaryResponseDTO> listarBots(Long userId);
+
+
+    BotResponseDTO obtenerBot(Long botId);
 }
