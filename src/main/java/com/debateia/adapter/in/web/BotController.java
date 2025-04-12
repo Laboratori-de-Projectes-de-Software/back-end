9b75560a0adc865d2f86892a8f6b51c91cb963ad
angelmartinez4 <angelmartinez200210@gmail.com>
2025-04-12 06:14:13 +0200

    PUT acabado, gestionando errores

--- src/main/java/com/debateia/adapter/in/web/BotController.java
+++ src/main/java/com/debateia/adapter/in/web/BotController.java
@@ -6,8 +6,10 @@ import com.debateia.adapter.in.web.dto.response.BotSummaryResponseDTO;
 import com.debateia.adapter.out.persistence.entities.BotEntity;
 import com.debateia.application.jwt.JwtService;
 import com.debateia.application.service.BotService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,7 +31,7 @@ public class BotController {
             @RequestParam(name = "owner", required = false) Integer ownerId) {
         if (!jwtService.isTokenValid(authenticate)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        } // @TODO actualizar gestion errores etc, revisar
         List<BotEntity> bots = botService.getBots(Optional.ofNullable(ownerId));
         List<BotSummaryResponseDTO> response = bots.stream()
                 .map(this::convertToSummaryDTO)
@@ -39,60 +41,48 @@ public class BotController {
 
     @PostMapping
     public ResponseEntity<BotResponseDTO> createBot(
-            @RequestHeader(HttpHeaders.AUTHORIZATION) String authenticate,
             @RequestBody BotDTO botDto) {
-        System.out.println("EL POST SE EJECUTA OK Y M***** Y J****");
-        //if (!jwtService.isTokenValid(authenticate)) {
-        //    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        //}
         BotEntity botEntity = toEntity(botDto);
         try {
             botEntity = botService.createBot(botEntity, botDto.getUserId()); // meter en BD si el usuario existe
-        } catch (IllegalArgumentException e) { // Bot ya existia
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)
-                    .build();
+        } catch (DataIntegrityViolationException e) { // bot ya existia
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (EntityNotFoundException e) { // anadir bot para usuario que no existe
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        if (botEntity == null) { // Caso: se queria anadir un bot para un usuario que no existe
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .build();
-        }
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(convertToResponseDto(botEntity));
+        return ResponseEntity.status(HttpStatus.CREATED).body(convertToResponseDto(botEntity));
     }
 
     @GetMapping("/{botId}")
     public ResponseEntity<BotResponseDTO> getBotById(
-            @RequestHeader(HttpHeaders.AUTHORIZATION) String authenticate,
             @PathVariable Integer botId) {
-        if (!jwtService.isTokenValid(authenticate)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        try {
+            BotEntity botEntity = botService.getBotById(botId);
+            return ResponseEntity.ok(convertToResponseDto(botEntity));
+        } catch (EntityNotFoundException e) { // bot no encontrado
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        BotEntity botEntity = botService.getBotById(botId);
-        if (botEntity == null) {
-            return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .build();
-        }
-        return ResponseEntity.ok(convertToResponseDto(botEntity));
     }
 
     @PutMapping("/{botId}")
     public ResponseEntity<BotResponseDTO> updateBot(
-            @RequestHeader(HttpHeaders.AUTHORIZATION) String authenticate,
             @PathVariable Integer botId,
             @RequestBody BotDTO botDto) {
-        if (!jwtService.isTokenValid(authenticate)) {
-            return ResponseEntity.
-                    status(HttpStatus.UNAUTHORIZED)
-                    .build();
-        }
         BotEntity botEntity = toEntity(botDto);
-        BotResponseDTO response = convertToResponseDto(botService.updateBot(botId, botEntity));
-        return ResponseEntity
-                .ok(response);
+        try {
+            BotEntity entityUpdate = botService.updateBot(botId, botDto.getUserId(), botEntity);
+            BotResponseDTO response = convertToResponseDto(entityUpdate);
+            return ResponseEntity.ok(response);
+        } catch (EntityNotFoundException e) { // bot/usuario asociado no existe
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (DataIntegrityViolationException e) { // usuario (es unico) seria repetido tras actualizar
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
     private BotSummaryResponseDTO convertToSummaryDTO(BotEntity bot) {
--- src/main/java/com/debateia/application/service/BotService.java
+++ src/main/java/com/debateia/application/service/BotService.java
@@ -5,9 +5,11 @@ import com.debateia.adapter.out.persistence.entities.UserEntity;
 import com.debateia.application.ports.out.persistence.BotRepository;
 import com.debateia.application.ports.out.persistence.UserRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -32,29 +34,41 @@ public class BotService {
 
         if (owner.isPresent()) { // usuario existe
             if (botRepository.exists(botEntity.getName())) { //
-                throw new IllegalArgumentException("El bot a añadir ya existe");
+                throw new DataIntegrityViolationException("Ya existe un bot con ese nombre");
             }
             botEntity.setUser(owner.get()); // anadir relacion del bot al usuario
             return botRepository.save(botEntity);
-        } else {
-            return null; // el controller trata el error
         }
+        throw new EntityNotFoundException("El usuario con ID "+userId+" no existe");
     }
 
     public BotEntity getBotById(Integer botId) {
         Optional<BotEntity> botEntity = botRepository.findById(botId);
-        return botEntity.orElse(null);
+        if (botEntity.isEmpty()) {
+            throw new EntityNotFoundException("El bot con ID "+botId+" no existe");
+        }
+        return botEntity.get();
     }
 
-    public BotEntity updateBot(Integer botId, BotEntity newBot) {
+    public BotEntity updateBot(Integer botId, Integer userId, BotEntity newBot) {
         Optional<BotEntity> currentBot = botRepository.findById(botId);
         if (currentBot.isEmpty()) {
-            return null;
+            throw new EntityNotFoundException("El bot con ID \""+botId+"\" a actualizar no existe");
+        }
+        Optional<UserEntity> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new EntityNotFoundException("El user con ID \""+userId+"\" propietario del bot no existe");
         }
         newBot.setWins(currentBot.get().getWins());
         newBot.setDraws(currentBot.get().getDraws());
         newBot.setLosses(currentBot.get().getLosses());
-        return botRepository.save(newBot);
+        newBot.setUser(user.get());
+        newBot.setId(botId); // IMPORTANTE!!! Para que save haga UPDATE en vez de INSERT
+        try {
+            return botRepository.save(newBot);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Ya existe un bot con nombre \""+newBot.getName()+"\"");
+        }
     }
 
 }
