3523e9f817e4532a9c579e2d0e9824622ec39eea
ivan <ivauib332@gmail.com>
2025-04-15 13:04:15 +0200

    Fixed all name errors

--- src/main/java/jaumesitos/backend/demo/infrastructure/db/repository/ParticipationDBORepository.java
+++ src/main/java/jaumesitos/backend/demo/infrastructure/db/repository/ParticipationDBORepository.java
@@ -6,6 +6,7 @@ import jaumesitos.backend.demo.domain.Participation;
 import jaumesitos.backend.demo.infrastructure.db.dbo.ParticipationDBO;
 import jaumesitos.backend.demo.infrastructure.db.mapper.ParticipationDBOMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ParticipationDBORepository implements IParticipationRepository {
+    @Qualifier("participationDBOMapper")
     private final ParticipationDBOMapper mapper;
     private final SpringDataParticipationRepository repository;
     @Override
