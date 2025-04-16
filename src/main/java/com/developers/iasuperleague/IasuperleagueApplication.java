eaafe8037a25c1fac7ec7b4997d25d9a25a25beb
Joan <joan.catala1@estudiant.uib.cat>
2025-04-16 16:34:13 +0200

    Arreglo

--- src/main/java/com/developers/iasuperleague/IasuperleagueApplication.java
+++ src/main/java/com/developers/iasuperleague/IasuperleagueApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.developers.iasuperleague;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class IasuperleagueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(IasuperleagueApplication.class, args);
 	}
 
 }
