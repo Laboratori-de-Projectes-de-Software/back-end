f9cce56db90939649d8470a2b71b2646633d7578
Cristobal <calbertimartinez@gmail.com>
2025-05-16 22:35:12 +0200

    Update BotMessageReceiverUserCase.java

--- src/main/java/com/debateia/application/ports/in/bot_messaging/BotMessageReceiverPort.java
+++
@@ -1,7 +0,0 @@
-package com.debateia.application.ports.in.bot_messaging;
-
-import com.debateia.domain.Messages;
-
-public interface BotMessageReceiverPort {
-    void receiveBotMessage(Messages response);
-}
