81ffb95324ca9cd5a0fa2fdc00d76085ae45dea5
peepbii10 <pepbiel2003@gmail.com>
2025-05-16 18:44:25 +0200

    change ports into allowedOrigins

--- src/main/java/com/debateia/config/CorsConfig.java
+++ src/main/java/com/debateia/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173") // TODO: make sure what websites to accept, in worse case, accept all
+            .allowedOrigins("http://localhost:80", "http://localhost")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
