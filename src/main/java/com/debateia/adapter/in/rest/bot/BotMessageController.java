de7d1dbb19ee80c966606d84d1751fa50b731b0e
Arturo <69714460+Jarturog@users.noreply.github.com>
2025-05-19 09:32:40 +0200

    haciendo que el backend envíe el mensaje a la API
    habría que ver cómo se quiere que maneje el primer mensaje de una match

--- src/main/java/com/debateia/adapter/in/rest/bot/BotMessageController.java
+++ src/main/java/com/debateia/adapter/in/rest/bot/BotMessageController.java
@@ -1,6 +1,9 @@
 package com.debateia.adapter.in.rest.bot;
 
+import com.debateia.adapter.out.bot_messaging.BotMessagingAdapter;
 import com.debateia.application.ports.in.rest.BotMessageReceiverUserCase;
+import com.debateia.application.ports.out.persistence.BotRepository;
+import com.debateia.domain.Bot;
 import com.debateia.domain.Messages;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import java.time.LocalDateTime;
 public class BotMessageController {
 
     private final BotMessageReceiverUserCase botMessageReceiver;
+    private final BotRepository botRepository;
+    private final BotMessagingAdapter botMessaging;
 
     @PostMapping("/{matchId}")
     public ResponseEntity<Void> receiveMessageFromBot(
@@ -26,6 +31,11 @@ public class BotMessageController {
         message.setMatchId(matchId);
 
         botMessageReceiver.receiveBotMessage(message);
+
+        Bot bot = botRepository.findById(message.getBotId()).orElseThrow();
+        String botApiUrl = bot.getEndpoint();
+        botMessaging.sendMessageToBot(message, botApiUrl);
+
         return ResponseEntity.ok().build();
     }
 }
